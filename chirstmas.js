f325482ddc68ef19145b07e154bdc5809a571ef4
Lorenzo Sinisi <lasslo.net@gmail.com>
2015-12-13 20:13:54 +0100

    added Looping a triangle and Fizzbuzz

---
+++ chirstmas.js
@@ -0,0 +1,12 @@
+// Write a program to Print:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+var string = '';
+for (var i = 0; i < 7; i++) {
+ console.log(string += '#');
+ }
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,6 @@
-# Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions.
-# For numbers divisible by 3, print "Fizz" instead of the number,
-# and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+// Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions.
+// For numbers divisible by 3, print "Fizz" instead of the number,
+// and for numbers divisible by 5 (and not 3), print "Buzz" instead.
 for (var i = 0; i < 100; i++){
   if ((i % 3) == 0)  {
       console.log("FIZZ");
