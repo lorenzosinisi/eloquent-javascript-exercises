2a053aa44523fdc2ff9bdb9a4903752869f12890
Lorenzo Sinisi <lasslo.net@gmail.com>
2015-12-14 14:44:23 +0100

    algorith of to find minimum in javascript including infinite

---
+++ minimum.js
@@ -0,0 +1,19 @@
+/*
+  Write a funciton that returns the minimum between 2 numbers in Javascript
+*/
+function minimum(first, second) {
+  if (typeof first != "number" || typeof second != "number" ) {
+    if (first == null && second == null) {
+      return -Infinity;
+    } else {
+      return null;
+    }
+  };
+  if (first < second) {
+    return first;
+  } else {
+    return second;
+  };
+}
+minimum(1, 100);
+minimum(); // -Infinity
